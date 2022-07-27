e5ef2b8e9eba65a7df486045bc5f3e63419d6ee6
João Gomes <gomesrodrigues.joaovitor6@gmail.com>
2022-07-27 19:29:06 -0300

    Fix exercicio

---
+++ script.js
@@ -0,0 +1,11 @@
+const arraystrings = ["Joao", "Gomes", "Rodrigues"]
+
+const arrayNumbers = [0, 1, 2]
+
+const arrayMisto = [2, "Barbosa", true]
+
+const arrayUmValor = [13]
+
+const arrayVazio = []
+
+console.log(arraystrings.length)
